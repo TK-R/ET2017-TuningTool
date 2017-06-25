c008f8e6caba908ead4a9c2f4df3bab08a7dbb2b
Ryosuke Koichi <subtkr@gmail.com>
2017-06-25 17:47:28 +0900

    運搬先にブロックが存在する場合にどける処理を実装

--- ET2017-TuningTool/Model/BlockMoveRule.cs
+++ ET2017-TuningTool/Model/BlockMoveRule.cs
@@ -12,10 +12,23 @@ namespace ET2017_TuningTool.Model
 
         BlockFieldModel FieldModel { get; set; }
         
+        int[] TargetNo { set; get; }
+
         public BlockMoveRule(EV3Model robot, BlockFieldModel field)
         {
             RobotModel = robot;
             FieldModel = field;
+
+            // 運搬先になりうるブロックのリスト
+            TargetNo = new int[]
+            {
+                5,
+                7,
+                8,
+                12,
+                13,
+            };
+
         }
 
         /// <summary>
@@ -25,37 +38,58 @@ namespace ET2017_TuningTool.Model
         public void ChangeNextPosition()
         {
             // 移動していない（=ブロック色と置き場の色が異なる置き場を検出）
-            var notMovedBlock = FieldModel. PlaceArray.Where(p => FieldModel.IsToMoveBlock(p.OnBlockColor, p.PlaceColor));
-
+            var notMovedBlock = FieldModel.PlaceArray
+                .Where(p => p.OnBlockColor != BlockColor.None)
+                .Where(p => !TargetNo.Contains(p.No) || FieldModel.IsToMoveBlock(p.OnBlockColor, p.PlaceColor));
+            
+            // 完了している場合
             if (notMovedBlock.Count() == 0)
                 return;
 
-            // 移動していないブロックのうち、ロボットの距離が最も近いブロックを対象とする
-            var srcPlace = notMovedBlock.FindMin(n => n.GetDistance(RobotModel.Position));
+            // 運搬先となりうる（上にブロックが置かれていない）置き場を列挙する    
+            var MoveAvailablePlace = FieldModel.PlaceArray
+                .Where(p => TargetNo.Contains(p.No))
+                .Where(p => p.OnBlockColor == BlockColor.None);
 
-            int dstNo = 0;
-            switch (srcPlace.OnBlockColor)
+            // 運搬可能なブロックを列挙する
+            var MoveAvailableBlock =
+                notMovedBlock.Where(b => 
+                    MoveAvailablePlace.Where(p => !FieldModel.IsToMoveBlock(p.PlaceColor,b.OnBlockColor)).Count() != 0);
+            
+            Place srcPlace ,dstPlace;
+            if (MoveAvailableBlock.Count() == 0) // 運搬可能なブロックがなく、すでに埋まっているケース
             {
-                case BlockColor.Black:
-                    dstNo = 7;
-                    break;
-                case BlockColor.Blue:
-                    dstNo = 8;
-                    break;
-                case BlockColor.Red:
-                    dstNo = 13;
-                    break;
-                case BlockColor.Yellow:
-                    dstNo = 12;
-                    break;
-                case BlockColor.Green:
-                    dstNo = 5;
-                    break;
-                default:
-                    return;
+                // 赤ブロック置き場で、違うブロックが乗っている個所を取得
+                var redP = FieldModel.PlaceArray
+                                     .Where(p => p.PlaceColor == BlockColor.Red)
+                                     .Where(p => p.OnBlockColor != BlockColor.Black)
+                                     .FindMin(p => p.GetDistance(RobotModel.Position));
+                if (redP != null)
+                {
+                    // 赤ブロック置き場を対象とする
+                    srcPlace = redP;
+                }
+                else
+                {
+                    // 異なる色のブロックが乗っており、かつ最も近い個所を対象とする　
+                    srcPlace = FieldModel.PlaceArray
+                                         .Where(p => p.OnBlockColor != p.PlaceColor)
+                                         .FindMin(p => p.GetDistance(RobotModel.Position));
+                }
+
+                // ブロックが置かれておらず、対象の置き場から最も近い場所を対象とする
+                dstPlace = FieldModel.PlaceArray
+                                  .Where(p => p.OnBlockColor == BlockColor.None)
+                                  .FindMin(p => p.GetDistance(redP.No));
+
             }
-            var dstPlace = FieldModel.PlaceArray.Where(p => p.OnBlockColor == BlockColor.None)
-                                     .Where(p => p.No == dstNo).First();
+            else // 運搬可能なブロックが存在するケース
+            {
+                // 移動していないブロックのうち、ロボットの距離が最も近いブロックを対象とする
+                srcPlace = MoveAvailableBlock.FindMin(n => n.GetDistance(RobotModel.Position));
+                dstPlace = MoveAvailablePlace.Where(p => TargetNo.Contains(p.No)).FindMin(p => p.GetDistance(RobotModel.Position));
+            }
+
 
             dstPlace.OnBlockColor = srcPlace.OnBlockColor;
             srcPlace.OnBlockColor = BlockColor.None;
